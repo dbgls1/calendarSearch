9521d6790de1d049df5cd687f25ae79567352d5f
IYS <dbgls1@naver.com>
2020-10-17 11:24:32 +0900

    5 - Twinkle Calendar
    img 폴더 추가
    쓰레드클래스를 활용하여 별이 반짝이는 배경 구현

--- src/CalendarSearchEx.java
+++ src/CalendarSearchEx.java
@@ -25,10 +25,8 @@ import javax.swing.SwingConstants;
 //https://hyeonstorage.tistory.com/205  ,참조
 public class CalendarSearchEx extends JFrame{
 	
-	//처음 시작화면을 현재 달력으로 표시하려면??
-		
 	static int startDay;
-	static int lastDay=31;
+	static int lastDay;
 	
 	public CalendarSearchEx(int startDay, int lastDay) {
 		
@@ -61,12 +59,75 @@ public class CalendarSearchEx extends JFrame{
 	public class PanelC extends JPanel{
 		
 		public PanelC() {
+					
+			setBackground(Color.black);
+			setLayout(null);
 			
-			ImageIcon img = new ImageIcon("img/night.jpg");
-			Image im = img.getImage();
-			JLabel la = new JLabel(img);
-			la.setSize(img.getIconWidth(), img.getIconHeight());
-			add(la);
+			for(int i=0;i<2000;i++) {
+				
+				int x= (int)(Math.random()*1920);
+				int y =(int)(Math.random()*1080);
+				
+				JLabel la=new JLabel("*");
+				la.setBackground(Color.BLACK);
+				la.setOpaque(true);
+				la.setSize(5, 5);
+				la.setLocation(x, y);
+				add(la);
+				//쓰레드
+				flicker f = new flicker(la);
+				f.start();				
+								
+				JLabel la2=new JLabel(".");
+				la2.setBackground(Color.BLACK);
+				la2.setOpaque(true);
+				la2.setSize(5, 5);
+				la2.setLocation(x, y);
+				add(la2);
+				flicker f2 = new flicker(la2);
+				f2.start();
+				
+				JLabel la3=new JLabel("+");
+				la3.setBackground(Color.BLACK);
+				la3.setOpaque(true);
+				la3.setSize(5, 5);
+				la3.setLocation(x, y);
+				add(la3);
+				flicker f3 = new flicker(la3);
+				f3.start();
+			}
+		}
+	}
+	
+	class flicker extends Thread{
+		
+		JLabel la;
+		
+		public flicker(JLabel la) {
+			this.la=la;
+		}
+		
+		@Override
+		public void run() {
+			
+			int n=0;
+			while(true) {
+				
+				if(n==0) {
+					la.setForeground(Color.white);
+					n++;
+				}else {
+					la.setForeground(Color.black);
+					n--;
+				}
+								
+				try {
+					Thread.sleep(1200);
+				} catch (Exception e) {
+					return;
+				}
+				
+			}
 			
 		}
 		
